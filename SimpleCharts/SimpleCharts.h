9d94c3fa24644f0d1af3a094e70f00339decb640
Juan Paolo  Del Rosario <juanpaolo@juans-mbp.broadband>
2019-01-24 16:47:52 +0000

    Initial Commit

---
+++ SimpleCharts/SimpleCharts.h
@@ -0,0 +1,19 @@
+//
+//  SimpleCharts.h
+//  SimpleCharts
+//
+//  Created by Juan Paolo  Del Rosario on 24/01/2019.
+//  Copyright © 2019 Juan Paolo Del Rosario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleCharts.
+FOUNDATION_EXPORT double SimpleChartsVersionNumber;
+
+//! Project version string for SimpleCharts.
+FOUNDATION_EXPORT const unsigned char SimpleChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleCharts/PublicHeader.h>
+
+
